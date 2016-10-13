c1c224a63da3f5cf4317ba32671ab63fe40584ce
IlonkaBuwalda <ilonka@equiavia.com>
2016-10-13 13:34:07 +1300

    Add project files.

---
+++ HorseDelux/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HorseDelux.Startup))]
+namespace HorseDelux
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
